559cceff71729dd401e8dddb27b375f553e3d55e
Nabil <113439816+nabilelbajdi@users.noreply.github.com>
2023-04-05 17:58:39 +0200

    Code for backup script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# backup-script
+Backup scrup in bash
+
+Given a source directory and a target, takes a backup of source to target.
+
+Handles targets both local or remote (ssh).
+
+Implemenmts incremental backups.
+
+Each new backup lives in a timestamped directory (i.e: the name of the directory is a timestamp).
---
+++ backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Function to create a timestamp
+get_timestamp() {
+  date +"%Y-%m-%d_%H-%M-%S"
+}
+
+# Set the source and target directories
+source_dir="/path/to/source/dir"
+target_dir="/path/to/target/dir"
+
+# Set the name of the backup directory to a timestamp
+backup_dir="$target_dir/$(get_timestamp)"
+
+# Create the backup directory
+mkdir -p "$backup_dir"
+
+# Perform an incremental backup using rsync
+rsync -a --delete --link-dest="$target_dir/latest" "$source_dir" "$backup_dir"
+
+# Update the symbolic link for latest backup
+ln -nsf "$backup_dir" "$target_dir/latest"
